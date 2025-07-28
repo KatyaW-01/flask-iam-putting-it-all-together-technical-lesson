0c3fc8201452e11df15bd32fb05ed71f174e0f5e
Katya Weicht <katya.weicht@gmail.com>
2025-07-28 16:51:43 -0600

    Create signup route

--- server/app.py
+++ server/app.py
@@ -5,6 +5,26 @@ from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
 
 from config import app, db, api
+from models import *
+
+class Signup(Resource):
+    def post(self):
+        request_json = request.get_json()
+        username = request_json.get('username')
+        password = request_json.get('password')
+
+        user = User(username=username)
+        user.password_hash = password
+
+        try:
+            db.session.add(user)
+            db.session.commit()
+            session['user_id'] = user.id
+            return UserSchema().dump(user), 201
+        except IntegrityError:
+            return {'error': '422 Unprocessable Entity'}, 422
+
+api.add_resource(Signup, '/signup', endpoint='signup')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
